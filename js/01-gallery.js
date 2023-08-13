06a490e0080da11bc70d99e147e3e6e0b0bbd346
Michał Viwegier <beebas@wp.pl>
2023-08-13 12:30:46 +0200

    Using 'basicLightbox' and 'SimpleLightbox' libraries

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+const dives = [];
+
+for (let i = 0; i < galleryItems.length; i++) {
+  const galleryItem = document.createElement("div");
+  const imageLink = `<a class="gallery__link" href="${galleryItems[i].original}">\n<img class="gallery__image" src="${galleryItems[i].preview}" data-source="${galleryItems[i].original}" alt="${galleryItems[i].description}" />\n</a>`;
+  galleryItem.classList.add("gallery__item");
+  dives.push(galleryItem);
+  gallery.append(...dives);
+  galleryItem.insertAdjacentHTML("afterbegin", imageLink);
+}
+
+document.querySelector(".gallery").onclick = (event) => {
+  event.preventDefault();
+  basicLightbox
+    .create(
+      `<img src="${event.target.dataset.source}" alt="${event.target.alt}">`
+    )
+    .show();
+};
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+const dives = [];
+
+for (let i = 0; i < galleryItems.length; i++) {
+  const galleryItem = document.createElement("div");
+  const imageLink = `<a class="gallery__link" href="${galleryItems[i].original}">\n<img class="gallery__image" src="${galleryItems[i].preview}" alt="${galleryItems[i].description}" />\n</a>`;
+  galleryItem.classList.add("gallery__item");
+  dives.push(galleryItem);
+  gallery.append(...dives);
+  galleryItem.insertAdjacentHTML("afterbegin", imageLink);
+}
+
+let lightbox = new SimpleLightbox(".gallery a", {
+  captionSelector: "img",
+  captionsData: "alt",
+  captionDelay: 250,
+});
