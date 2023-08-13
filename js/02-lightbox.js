0d03fdfe583b4b3156d8dd34cfaafb4608524864
Michał Viwegier <beebas@wp.pl>
2023-08-13 20:52:31 +0200

    Rename 'dives' to 'items'

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,16 +2,16 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 const gallery = document.querySelector(".gallery");
-const dives = [];
+const items = [];
 
 for (let i = 0; i < galleryItems.length; i++) {
   const galleryItem = document.createElement("div");
   const imageLink = `<a class="gallery__link" href="${galleryItems[i].original}">\n<img class="gallery__image" src="${galleryItems[i].preview}" alt="${galleryItems[i].description}" />\n</a>`;
   galleryItem.classList.add("gallery__item");
-  dives.push(galleryItem);
+  items.push(galleryItem);
   galleryItem.insertAdjacentHTML("afterbegin", imageLink);
 }
-gallery.append(...dives);
+gallery.append(...items);
 
 let lightbox = new SimpleLightbox(".gallery a", {
   captionSelector: "img",
